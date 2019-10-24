26a51160b80ccd6566c8064dcc57efaa0089e2fe
bejolo018 <bejolo018@gmail.com>
2019-10-23 22:20:34 -0500

    started adding models

--- adventure/admin.py
+++ adventure/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+class RoomAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+
+
+class PlayerAdmin(admin.ModelAdmin):
+    readonly_fields = ('id', 'uuid')
+
+
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Player, PlayerAdmin)
