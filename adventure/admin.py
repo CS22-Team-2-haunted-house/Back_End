fe26abf0764a1f075b2a5038dfea795b72fa005e
Mitchell Robles <mitchellr92@live.com>
2019-10-24 09:45:05 -0700

    accepted incoming changes

--- adventure/admin.py
+++ adventure/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
-<<<<<<< HEAD
-
 from .models import Room, Player
-
-
-=======
-from .models import Room, Player
->>>>>>> 716b15a33aed978ded8a6bde17855cb6c6aa7f78
 # Register your models here.
 class RoomAdmin(admin.ModelAdmin):
     readonly_fields = ('id',)
@@ -17,8 +10,4 @@ class PlayerAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Room, RoomAdmin)
-<<<<<<< HEAD
 admin.site.register(Player, PlayerAdmin)
-=======
-admin.site.register(Player, PlayerAdmin)
->>>>>>> 716b15a33aed978ded8a6bde17855cb6c6aa7f78
